a090e7868d737924667c6773eb4f3900f67bba7f
Shadowfacts <me@shadowfacts.net>
2018-11-16 11:13:25 -0500

    Sum of Multiples

---
+++ sum-of-multiples/sum_of_multiples.exs
@@ -0,0 +1,15 @@
+defmodule SumOfMultiples do
+  @doc """
+  Adds up all numbers from 1 to a given end number that are multiples of the factors provided.
+  """
+  @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
+  def to(limit, factors) do
+  	1..limit - 1
+  		|> Enum.filter(fn x -> factor?(x, factors) end)
+  		|> Enum.sum
+  end
+
+  def factor?(n, factors) do
+  	Enum.any?(factors, fn f -> rem(n, f) == 0 end)
+  end
+end
